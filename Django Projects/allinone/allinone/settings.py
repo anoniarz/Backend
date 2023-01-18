d1deb960230836cc86d568b2b43d3e044e6d9ae8
anoniarz <s3nkjuu@wp.pl>
2023-01-18 15:15:05 +0100

    a

--- Django Projects/allinone/allinone/settings.py
+++ Django Projects/allinone/allinone/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
     'home',
     'mockaroo',
     'django.contrib.admin',
--- Django Projects/allinone/allinone/urls.py
+++ Django Projects/allinone/allinone/urls.py
@@ -5,8 +5,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('blog/', include('blog.urls')),
     path('', include('home.urls')),
     path('mockaroo/', include('mockaroo.urls')),
     path('admin/', admin.site.urls),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
