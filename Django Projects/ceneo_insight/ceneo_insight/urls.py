f1849be413854143fb98ab5262e748fc0202d1fb
anoniarz <s3nkjuu@wp.pl>
2023-03-13 12:46:38 +0100

    cos

--- Django Projects/ceneo_insight/ceneo_insight/urls.py
+++ Django Projects/ceneo_insight/ceneo_insight/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', include('main.urls')),
 
     path('register/', user_views.register, name='register'),
-    path('profile/', user_views.profile, name='profile'),
+    path('favourites/', user_views.favourites, name='favourites'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
--- Django Projects/ceneo_insight/users/forms.py
+++ Django Projects/ceneo_insight/users/forms.py
@@ -19,8 +19,3 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
-
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['image']
