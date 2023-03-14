308a3aedf7e31587ba106a7d7975c1aa57d24670
anoniarz <s3nkjuu@wp.pl>
2023-03-14 11:54:58 +0100

    zmiany

--- Django Projects/ceneo_insight/users/views.py
+++ Django Projects/ceneo_insight/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm
+from .forms import UserRegisterForm
 from .models import Profile
 from main.views import Product
 
@@ -26,42 +26,38 @@ def favourites(request):
     sort_by = request.GET.get('sort_by')
     profile = request.user.profile
     favourites = profile.favourites.all()
-    products = Product.objects.all()
-    
-    if sort_by == 'a-z':
-        products = products.order_by('product_name')
-    elif sort_by == 'z-a':
-        products = products.order_by('-product_name')
-    elif sort_by == 'most_reviews':
-        products = sorted(products, key=lambda p: p.reviews.count(), reverse=True)
-    elif sort_by == 'least_reviews':
-        products = sorted(products, key=lambda p: p.reviews.count())
-    elif sort_by == 'highest_rating':
-        products = products.order_by('-rating')
-    elif sort_by == 'lowest_rating':
-        products = products.order_by('rating')
         
+    if sort_by == 'a-z':
+        favourites = favourites.order_by('product_name')
+    elif sort_by == 'z-a':
+        favourites = favourites.order_by('-product_name')
+    elif sort_by == 'most_reviews':
+        favourites = sorted(favourites, key=lambda p: p.reviews.count(), reverse=True)
+    elif sort_by == 'least_reviews':
+        favourites = sorted(favourites, key=lambda p: p.reviews.count())
+    elif sort_by == 'highest_rating':
+        favourites = favourites.order_by('-rating')
+    elif sort_by == 'lowest_rating':
+        favourites = favourites.order_by('rating')
     request.session['sort_by'] = sort_by
     
     context = {
         'favourites': favourites,
-        'produxts': products,
         "sort_by": sort_by,
     }
     
     return render(request, 'users/favourites.html', context)
 
+@login_required
 def add_to_favourites(request, pk):
     product = Product.objects.get(product_id=pk)
-    
     profile = request.user.profile
     profile.add_to_favourites(product)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-
+@login_required
 def remove_from_favourites(request, pk):
     product = get_object_or_404(Product, product_id=pk)
-    if request.user.is_authenticated:
-        profile = get_object_or_404(Profile, user=request.user)
-        profile.favourites.remove(product)
+    profile = get_object_or_404(Profile, user=request.user)
+    profile.favourites.remove(product)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
